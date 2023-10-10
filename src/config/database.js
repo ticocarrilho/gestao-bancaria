f6de5f40c11830700a206b8663c030b6da098d24
Thiago Carrilho <ticocarrilho@gmail.com>
2023-10-10 13:37:08 -0300

    adiciona configurações de DB para ambientes de prod e test

--- src/config/database.js
+++ src/config/database.js
@@ -13,4 +13,28 @@ module.exports = {
       underscored: false,
     },
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'tests',
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: console.log,
+    define: {
+      timestamps: true,
+      underscored: false,
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: false,
+    },
+  },
 };
