a0fc79bd76ad87cdade708276bd62853a12bcf9e
Thiago Carrilho <ticocarrilho@gmail.com>
2023-10-11 14:53:46 -0300

    adiciona testes unitarios de conta]

--- __tests__/unit/conta.test.js
+++ __tests__/unit/conta.test.js
@@ -1,3 +1,37 @@
+const { Conta } = require('../../src/app/models')
+
+const dadosConta = {
+  saldo: 100,
+  conta_id: Math.random()
+}
+
 describe('Conta', () => {
-  
-})
+  it('deve criar uma conta sem erro', async () => {
+    await expect(Conta.create(dadosConta)).resolves.not.toThrowError();
+  })
+
+
+  it('deve dar erro ao criar uma conta sem a propriedade de saldo', async () => {
+    const { conta_id } = dadosConta;
+    await expect(Conta.create({ conta_id })).rejects.toThrowError();
+  })
+
+  it('deve dar erro ao criar uma conta sem a propriedade de conta_id', async () => {
+    const { saldo } = dadosConta;
+    await expect(Conta.create({ saldo })).rejects.toThrowError();
+  })
+
+  it('deve dar erro ao criar uma conta com tipo de saldo incorreto', async () => {
+    await expect(Conta.create({ ...dadosConta, saldo: 'a'})).rejects.toThrowError();
+  })
+
+  it('deve dar erro ao criar uma conta com tipo de saldo negativo', async () => {
+    await expect(Conta.create({ ...dadosConta, saldo: -1 })).rejects.toThrowError();
+  })
+
+  it('deve dar erro ao criar uma conta com um conta_id já existente', async () => {
+    await expect(Conta.create(dadosConta)).resolves.not.toThrowError();
+    await expect(Conta.create(dadosConta)).rejects.toThrowError();
+  })
+
+});
