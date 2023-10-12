af4c698f68ce2513f90569c281cc995d852342d9
Thiago Carrilho <ticocarrilho@gmail.com>
2023-10-12 16:07:10 -0300

    adiciona funcionalidades de transação

--- src/routes/transacaoRoutes.js
+++ src/routes/transacaoRoutes.js
@@ -2,4 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const TransacaoController = require('../app/controller/transacaoController');
 
+routes.post(
+  '/',
+  TransacaoController.store
+);
+
 module.exports = routes;
